4730e8ae78bf8e971c9e5b03d3f42773f13b08c7
mashuai <13935826473@163.com>
2020-12-01 20:46:08 +0800

    马帅改

--- src/main/java/com/baizhi/GitShrioApplication.java
+++ src/main/java/com/baizhi/GitShrioApplication.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitShrioApplication {
 
     public static void main(String[] args) {
+
+        String a = "二百大傻逼";
+
         SpringApplication.run(GitShrioApplication.class, args);
     }
 
